6d7707f3b19e5c1190951e2360c2fbe87161ad94
João Antunes <joaofbantunes@gmail.com>
2019-01-20 09:35:35 +0000

    Migrating database on app startup

---
+++ src/CodingMilitia.PlayBall.Auth.Web/StartupHelpers/DatabaseExtensions.cs
@@ -0,0 +1,20 @@
+using System.Threading.Tasks;
+using CodingMilitia.PlayBall.Auth.Web.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+// ReSharper disable once CheckNamespace - by design, to ease discoverability 
+namespace Microsoft.AspNetCore.Hosting
+{
+    internal static class DatabaseExtensions
+    {
+        internal static async Task EnsureDbUpToDateAsync(this IWebHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetRequiredService<AuthDbContext>();
+                await context.Database.MigrateAsync();
+            }
+        }
+    }
+}
