9352827278304ed082ee4ba39b444563867f4334
João Antunes <joaofbantunes@gmail.com>
2019-02-10 12:41:55 +0000

    Taking a look at internationalization

--- src/CodingMilitia.PlayBall.Auth.Web/Startup.cs
+++ src/CodingMilitia.PlayBall.Auth.Web/Startup.cs
@@ -1,19 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using CodingMilitia.PlayBall.Auth.Web.Data;
+﻿using CodingMilitia.PlayBall.Auth.Web.Data;
 using CodingMilitia.PlayBall.Auth.Web.Utilities;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Globalization;
+using System.Threading.Tasks;
 
 namespace CodingMilitia.PlayBall.Auth.Web
 {
@@ -30,19 +31,35 @@ namespace CodingMilitia.PlayBall.Auth.Web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddLocalization(options => options.ResourcesPath = "Resources");
+
             services
                 .AddMvc()
                 .SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
                 .AddRazorPagesOptions(options =>
                 {
                     options.Conventions.AuthorizeFolder("/Account");
-                });
-            
+                })
+                .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix)
+                .AddDataAnnotationsLocalization();
+
+            services.Configure<RequestLocalizationOptions>(options =>
+            {
+                var cultures = new[]
+                {
+                    new CultureInfo("en"),
+                    new CultureInfo("pt")
+                };
+                options.DefaultRequestCulture = new RequestCulture("en");
+                options.SupportedCultures = cultures;
+                options.SupportedUICultures = cultures;
+            });
+
             services.AddDbContext<AuthDbContext>(options =>
             {
                 options.UseNpgsql(_configuration.GetConnectionString("AuthDbContext"));
             });
-            
+
 
             services
                 .AddIdentity<PlayBallUser, IdentityRole>(options =>
@@ -79,6 +96,7 @@ namespace CodingMilitia.PlayBall.Auth.Web
             app.UseHsts(); // https://www.owasp.org/index.php/HTTP_Strict_Transport_Security_Cheat_Sheet
             app.UseHttpsRedirection(); // if a request comes in HTTP, it's redirect to HTTPS
             app.UseStaticFiles();
+            app.UseRequestLocalization(app.ApplicationServices.GetRequiredService<IOptions<RequestLocalizationOptions>>().Value);
             app.UseAuthentication();
             app.UseMvcWithDefaultRoute();
         }
