55b96657714a72783833fecd21577fbbe6fbcd0a
João Antunes <joao@antunes.dev>
2020-03-28 18:46:46 +0000

    Detecting/inferring events on SaveChanges
    Unrelated, also took the chance to do a small DI configuration cleanup

--- src/CodingMilitia.PlayBall.Auth.Web/Data/AuthDbContext.cs
+++ src/CodingMilitia.PlayBall.Auth.Web/Data/AuthDbContext.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -5,15 +8,32 @@ namespace CodingMilitia.PlayBall.Auth.Web.Data
 {
     public class AuthDbContext : IdentityDbContext<PlayBallUser>
     {
-        public AuthDbContext(DbContextOptions<AuthDbContext> options)
+        private readonly IEnumerable<IEventDetector> _eventDetectors;
+
+        public AuthDbContext(DbContextOptions<AuthDbContext> options, IEnumerable<IEventDetector> eventDetectors)
             : base(options)
         {
+            _eventDetectors = eventDetectors;
         }
         
         protected override void OnModelCreating(ModelBuilder builder)
         {
             builder.HasDefaultSchema("public");
             base.OnModelCreating(builder);
+        }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            foreach (var detector in _eventDetectors)
+            {
+                detector.Detect(this);
+            }
+            
+            // TODO: publish detected events
+            
+            var result = await base.SaveChangesAsync(cancellationToken);
+
+            return result;
         }
     }
 }
