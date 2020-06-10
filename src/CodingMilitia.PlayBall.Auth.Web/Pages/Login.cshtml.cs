a7452afd3eaa290c40f834cb1087f725caff5f29
João Antunes <joao@antunes.dev>
2020-06-10 15:31:19 +0100

    Fix SharedResources due to .NET 3.0 breaking change
    More info at https://github.com/dotnet/aspnetcore/issues/17733

--- src/CodingMilitia.PlayBall.Auth.Web/Pages/Login.cshtml.cs
+++ src/CodingMilitia.PlayBall.Auth.Web/Pages/Login.cshtml.cs
@@ -1,7 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 using CodingMilitia.PlayBall.Auth.Web.Data;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
